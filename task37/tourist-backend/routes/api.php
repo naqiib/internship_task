0dfc8519e8140e780d724300e46fd9d767ee0caf
naqiib <naqeeebknn@gmail.com@email.com>
2026-09-19 19:49:23 +0500

    api update

--- task37/tourist-backend/routes/api.php
+++ task37/tourist-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AdminController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookingController;
